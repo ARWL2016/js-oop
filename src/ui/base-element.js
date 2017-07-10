d20453324e8dacd1bb2c7ddc6e2a4f49bffbfea2
Alistair Willis <alistairrwillis@gmail.com>
2017-07-10 14:18:20 +0900

    add UI base class

---
+++ src/ui/base-element.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+
+export class BaseElement {
+
+  constructor() {
+    this.element = null; // jQuery wrapped object 
+  }
+
+  appendToElement(el) {
+    this.createElement(); 
+    el.append(this.element);
+    this.enableJS();
+  }
+
+  createElement() {
+    let s = this.getElementString(); 
+    this.element = $(s); 
+  }
+
+  getElementString() {
+    throw 'Please override getElementString() in BaseElement';
+    // this error tells the dev they must define this method
+  }
+
+  enableJS() {
+    componentHandler.upgradeElement(this.element[0]);
+  }
+
+}
